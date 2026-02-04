a8e3defc9020a550109b0ab6020cab70ea710de1
shsmith330 <shsmith@student.neumont.edu>
2026-02-03 19:28:28 -0600

    Completed Solo Project 1

--- src/edu/jsu/mcis/cs310/Converter.java
+++ src/edu/jsu/mcis/cs310/Converter.java
@@ -1,9 +1,22 @@
 package edu.jsu.mcis.cs310;
 
-import com.github.cliftonlabs.json_simple.*;
-import com.opencsv.*;
+import com.opencsv.CSVReaderHeaderAware;
+import java.util.*;
+import java.io.StringReader;
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.JsonException;
+import com.github.cliftonlabs.json_simple.JsonObject;
+import com.github.cliftonlabs.json_simple.Jsoner;
+import com.opencsv.CSVWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+
 
 public class Converter {
+    String csvFilePath = "input.csv"; // Replace with your file path
+    String jsonFilePath = "input.json";
+    String jsonOutput = csvToJson(csvFilePath);
+    String csvOutput = jsonToCsv(jsonFilePath);
     
     /*
         
@@ -73,12 +86,42 @@ public class Converter {
     
     @SuppressWarnings("unchecked")
     public static String csvToJson(String csvString) {
+        JsonArray jArray = new JsonArray();
+        //System.out.println(csvString);
+        JsonObject finished = new JsonObject();
+      
+       
         
         String result = "{}"; // default return value; replace later!
         
         try {
+        CSVReaderHeaderAware reader = new CSVReaderHeaderAware(new StringReader(csvString));
+        List<String[]> csvArray = reader.readAll();
+        String[] headers = csvArray.get(0);
+        JsonArray headings = new JsonArray();
+        JsonArray rowData = new JsonArray();
+        JsonArray pNums = new JsonArray();
         
-            // INSERT YOUR CODE HERE
+        headings.addAll(Arrays.asList(headers));
+         
+        
+        for (int i = 1; i <csvArray.size(); i++){
+            String[] row = csvArray.get(i);
+            pNums.add(row[0]);
+            JsonObject Text = new JsonObject();
+            for(int x = 0; x < headers.length && x< row.length; x++){
+                Text.put(headers[x], row[x]);
+                
+            }
+            rowData.add(Text);
+        }
+          
+        finished.put("ColHeadings", headings);
+        finished.put("ProdNums", pNums);
+        finished.put("Data", rowData);
+        
+        
+        return Jsoner.serialize(finished);
             
         }
         catch (Exception e) {
@@ -91,15 +134,102 @@ public class Converter {
     
     @SuppressWarnings("unchecked")
     public static String jsonToCsv(String jsonString) {
-        
+// "ProdNum","Title","Season","Episode","Stardate","OriginalAirdate","RemasteredAirdate"
+// "6149-02","Where No Man Has Gone Before","1","01","1312.4 - 1313.8","9/22/1966","1/20/2007"
         String result = ""; // default return value; replace later!
         
         try {
             
-            // INSERT YOUR CODE HERE
+          JsonObject mainObj = (JsonObject)Jsoner.deserialize(jsonString);
+          StringWriter sWriter = new StringWriter();
+          
+          try(CSVWriter csvWriter = new CSVWriter(sWriter)){
+              JsonArray headings = (JsonArray)mainObj.get("ColHeadings");
+              JsonArray data = (JsonArray)mainObj.get("Data");
+              JsonArray pNums = (JsonArray)mainObj.get("ProdNums");
+              
+              if (headings != null) {
+        String[] headerRow = headings.stream()
+                .map(String::valueOf)
+                .toArray(String[]::new);
+        csvWriter.writeNext(headerRow);
+    }
+            for (Object rowObj : data) {
+                if (rowObj instanceof JsonArray & pNums != null) {
+                    JsonArray row = (JsonArray) rowObj;
+                    String[] entries = new String[row.size()];
+                    String[] numRow = new String[pNums.size()];
+                    List<String> fullLine = new ArrayList<>();
+                    
+                    
+                    for (int i = 0; i < row.size(); i++) {
+                        entries[i] = String.valueOf(row.get(i));
+                        numRow[i] = String.valueOf(pNums.get(i));
+                        fullLine.add(numRow[i]);
+                        fullLine.add(entries[i]);
+        
+        }
+        csvWriter.writeNext(fullLine.toArray(new String[0]));
+    }
+}
+      
+              
+              //I used these lists to parse the JsonArrays
+//              List<String> dataList = new ArrayList<>();
+//              for(Object item : data){
+//                  dataList.add((String)item);
+//              }
+//              List<String> numList = new ArrayList<>();
+//              for(Object item : pNums){
+//                  numList.add((String)item);
+//              }
+
+
+
+              
+//              System.out.println(headings);
+//              System.out.println(data);
+//              System.out.println(pNums);
+              //System.out.println(dataList);
+              
+//              String[] headers = headings.toArray(new String[0]);
+              //String[] text = data.toArray(new String[0]);
+//              String[] nums = pNums.toArray(new String[1]);
+// Created these thinking they would work, they in fact, did not.
+              
+              //System.out.println(sWriter.toString());
+//              for (Object text : data) {
+//                  System.out.println(text);
+//                  System.out.println(nums.toString());
+//                  Object fullData = new Object();
+//                  for(int i = 0; i < data.size(); i++){
+//                      
+//                  }
+//              }
+//              for(Object text : data){
+//                  if (!(text instanceof JsonObject)) continue;
+//              JsonObject rows = (JsonObject)text;
+//              String[] row = new String[headers.length]; // This one was wierd because its one of the only . commands that doesnt have () at the end.
+//                for (int i = 0; i < headers.length; i++) {
+//    // 1. Get the value as a generic Object (could be Integer, String, or Boolean)
+//    Object val = rows.get(headers[i]);
+//                    System.out.println("carl" + i);
+//
+//    // 2. Convert to String safely
+//    // If it's an int (e.g., 1), .toString() makes it "1"
+//    // If it's null, we use "" to avoid a crash
+//    row[i] = (val != null) ? String.valueOf(val) : "";
+//}
+//                csvWriter.writeNext(row, true);
+//              }
+              
+          }
+          String finalCSV = sWriter.toString();
+          return finalCSV;
+            
             
         }
-        catch (Exception e) {
+        catch (JsonException | IOException e) {
             e.printStackTrace();
         }
         
